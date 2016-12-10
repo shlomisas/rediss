754d2cf7ac2a3e1357d48d17ddb666d5cb90edbe
Shlomi Sasson <shlomi@sasson.me>
2016-12-10 23:14:07 +0200

    Exposed index file to package

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Shlomi
+ */
+
+import SimpleObject from './src/types/redisSimpleObject';
+import Set from './src/types/redisSet';
+import Hash from './src/types/redisHash';
+
+export const RedisSimpleObject = SimpleObject;
+export const RedisSet = Set;
+export const RedisHash = Hash;
